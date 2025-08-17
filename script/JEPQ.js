5028d50315ee95640dc5233c09b04ff2bbd52748
JEPQFTW <dgtelia3@gmail.com>
2025-08-17 09:36:57 +0100

    Update JEPQ.js

--- script/JEPQ.js
+++ script/JEPQ.js
@@ -1,4 +1,4 @@
-<script>
+
 const buckets = [
     { id: 'options', prefix: '/JEPQ/data/JEPQ_Options_-_Index_' },
     { id: 'cash',    prefix: '/JEPQ/data/JEPQ_Cash_' },
@@ -133,4 +133,4 @@ document.querySelectorAll('th').forEach(th => {
         rows.forEach(row => tbody.appendChild(row));
     });
 });
-</script>
+
