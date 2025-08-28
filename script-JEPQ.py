aeb20fe499b431abce630c230e3c680ba3e43177
JEPQFTW <dgtelia3@gmail.com>
2025-08-28 20:44:21 +0100

    Update script-JEPQ.py

--- script-JEPQ.py
+++ script-JEPQ.py
@@ -91,7 +91,7 @@ def main():
                 lambda val: pd.Series(parse_option_info(val))
             )
 
-            opening_price = CONFIG["NDX]  # hardcoded underlying price
+            opening_price = CONFIG["NDX"]  # hardcoded underlying price
 
             # Calculate Contracts
             subset['Contracts'] = -subset['BaseMV'] / subset['Price']
